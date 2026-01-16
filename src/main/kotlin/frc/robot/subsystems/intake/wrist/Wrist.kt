358b29f562583b377e5bd2606cd96d6c06a33c0a
rakrakon <rakrakon@gmail.com>
2026-01-16 12:03:23 +0200

    add absolute encoder

--- src/main/kotlin/frc/robot/subsystems/intake/wrist/Wrist.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/wrist/Wrist.kt
@@ -1,10 +1,12 @@
 package frc.robot.subsystems.intake.wrist
 
 import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.hardware.CANcoder
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.lib.extensions.deg
 import frc.robot.lib.universal_motor.UniversalTalonFX
+import org.littletonrobotics.junction.AutoLogOutput
 import org.littletonrobotics.junction.mechanism.LoggedMechanism2d
 import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d
 
@@ -14,15 +16,24 @@ object Wrist : SubsystemBase(), WristPositionsCommandFactory {
             port = PORT,
             config = CONFIG,
             simGains = SIM_GAINS,
+            absoluteEncoderOffset = ABSOLUTE_ENCODER_OFFSET,
             gearRatio = GEAR_RATIO
         )
 
+    @AutoLogOutput
     private var setPoint = 0.deg
 
+    private val encoder = CANcoder(ENCODER_ID)
+
+    init {
+        encoder.configurator.apply(ENCODER_CONFIG)
+    }
+
+    @AutoLogOutput(key = "Wrist/mechanism")
     private var mechanism = LoggedMechanism2d(5.0, 5.0)
-    private var root = mechanism.getRoot("Hood", 2.5, 2.5)
+    private var root = mechanism.getRoot("Wrist", 2.5, 2.5)
     private val ligament =
-        root.append(LoggedMechanismLigament2d("HoodLigament", 1.0, 0.0))
+        root.append(LoggedMechanismLigament2d("WristLigament", 1.0, 0.0))
 
     private val positionRequest = PositionVoltage(0.deg)
     override fun setTarget(value: WristPositions): Command {
@@ -30,6 +41,7 @@ object Wrist : SubsystemBase(), WristPositionsCommandFactory {
             setPoint = value.angle
             motor.setControl(positionRequest.withPosition(value.angle))
         }
+
     }
 
     override fun periodic() {
--- src/main/kotlin/frc/robot/subsystems/intake/wrist/WristConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/wrist/WristConstants.kt
@@ -1,15 +1,20 @@
 package frc.robot.subsystems.intake.wrist
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration
 import com.ctre.phoenix6.configs.FeedbackConfigs
 import com.ctre.phoenix6.configs.MotorOutputConfigs
 import com.ctre.phoenix6.configs.Slot0Configs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
+import com.ctre.phoenix6.signals.SensorDirectionValue
 import edu.wpi.first.units.measure.Angle
 import frc.robot.lib.Gains
 import frc.robot.lib.createCurrentLimits
 import frc.robot.lib.extensions.deg
+import frc.robot.lib.extensions.get
+import frc.robot.lib.extensions.rad
+import frc.robot.lib.extensions.rot
 import org.team5987.annotation.command_enum.CommandEnum
 
 val PORT = 0
@@ -22,6 +27,20 @@ val SIM_GAINS = Gains(kP = 1.33, kD = 0.24)
 
 val REAL_GAINS = Gains(kP = 1.0, kD = 0.0)
 // TODO: actual values
+
+val ENCODER_ID = 10
+//TODO: actual value
+
+val ABSOLUTE_ENCODER_OFFSET = 0.rad
+//TODO: actual value
+
+val ENCODER_CONFIG =
+    CANcoderConfiguration().apply {
+        MagnetSensor.SensorDirection =
+            SensorDirectionValue.CounterClockwise_Positive
+        MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.9
+        MagnetSensor.MagnetOffset = ABSOLUTE_ENCODER_OFFSET[rot]
+    }
 
 val CONFIG =
     TalonFXConfiguration().apply {
