86c543e07d1158f7bcdc5d8c3442594d9296171f
Hadas <bar.hadas.bar@gmail.com>
2026-01-16 11:47:07 +0200

    Create genral fun of auto

---
+++ src/main/kotlin/frc/robot/autonomous/AutoCommands.kt
@@ -0,0 +1,11 @@
+package frc.robot.autonomous
+
+import com.pathplanner.lib.auto.AutoBuilder
+import com.pathplanner.lib.path.PathPlannerPath
+import edu.wpi.first.wpilibj2.command.Command
+
+internal fun runPath(name: String): Command {
+val path = PathPlannerPath.fromPathFile(name)
+val startPose= path.pathPoses[0]
+return AutoBuilder.resetOdom(startPose).andThen(AutoBuilder.followPath(path))
+}
