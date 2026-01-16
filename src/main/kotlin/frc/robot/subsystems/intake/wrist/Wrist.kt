3a858ad34dd129678563e49f7c28102ba1b9b756
rakrakon <rakrakon@gmail.com>
2026-01-16 10:25:31 +0200

    continue (not finished)

--- src/main/kotlin/frc/robot/subsystems/intake/wrist/Wrist.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/wrist/Wrist.kt
@@ -3,13 +3,16 @@ package frc.robot.subsystems.intake.wrist
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.lib.universal_motor.UniversalTalonFX
 
-object Wrist: SubsystemBase() {
-    private val motor = UniversalTalonFX(
-        port = PORT,
-        config = CONFIG,
-        simGains = SIM_GAINS,
-        gearRatio = GEAR_RATIO
-    )
+object Wrist : SubsystemBase() {
+    private val motor =
+        UniversalTalonFX(
+            port = PORT,
+            config = CONFIG,
+            simGains = SIM_GAINS,
+            gearRatio = GEAR_RATIO
+        )
 
+    override fun WristPositionsCommandFactory(){
 
+    }
 }
--- src/main/kotlin/frc/robot/subsystems/intake/wrist/WristConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/wrist/WristConstants.kt
@@ -1,37 +1,47 @@
 package frc.robot.subsystems.intake.wrist
 
-import com.ctre.phoenix6.configs.FeedbackConfigs
 import com.ctre.phoenix6.configs.MotorOutputConfigs
 import com.ctre.phoenix6.configs.Slot0Configs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
+import edu.wpi.first.units.measure.Angle
 import frc.robot.lib.Gains
 import frc.robot.lib.createCurrentLimits
+import frc.robot.lib.extensions.deg
+import org.team5987.annotation.command_enum.CommandEnum
 
 val PORT = 0
-//TODO: actual port
+// TODO: actual port
 
 val GEAR_RATIO = 1.0
-//TODO: actual value
+// TODO: actual value
 
 val SIM_GAINS = Gains(kP = 1.0, kD = 0.0)
-//TODO: actual values
+// TODO: actual values
 
-val CONFIG = TalonFXConfiguration().apply {
-    MotorOutput =
-        MotorOutputConfigs().apply {
-            NeutralMode = NeutralModeValue.Brake
-            Inverted = InvertedValue.Clockwise_Positive
-            // TODO: check motor direction
-        }
+val CONFIG =
+    TalonFXConfiguration().apply {
+        MotorOutput =
+            MotorOutputConfigs().apply {
+                NeutralMode = NeutralModeValue.Brake
+                Inverted = InvertedValue.Clockwise_Positive
+                // TODO: check motor direction
+            }
 
-    CurrentLimits = createCurrentLimits()
+        CurrentLimits = createCurrentLimits()
 
-    Slot0 =
-        Slot0Configs().apply {
-            kP = 1.0
-            kD = 0.0
-            // TODO: actual values
-        }
+        Slot0 =
+            Slot0Configs().apply {
+                kP = 1.0
+                kD = 0.0
+                // TODO: actual values
+            }
+    }
+
+@CommandEnum
+enum class WristPositions(angle: Angle) {
+    INTAKE(0.deg),
+    UP(30.deg)
+    // TODO: actual values
 }
