54c67cfbd772d7e799e854b088625b0f4bb5d5c4
AdarWa <adar.wasserman@gmail.com>
2026-01-18 18:08:56 +0200

    Fix Spindexer

--- src/main/kotlin/frc/robot/subsystems/netConveyor/Spindexer.kt
+++ src/main/kotlin/frc/robot/subsystems/netConveyor/Spindexer.kt
@@ -1,21 +1,16 @@
-package frc.robot.subsystems.conveyor.netConveyor
+package frc.robot.subsystems.netConveyor
 
-import com.ctre.phoenix6.controls.Follower
 import com.ctre.phoenix6.controls.VelocityVoltage
 import com.ctre.phoenix6.controls.VoltageOut
-import com.ctre.phoenix6.signals.MotorAlignmentValue
-import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
-import frc.robot.lib.sysid.SysIdable
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.littletonrobotics.junction.Logger
 import org.team5987.annotation.LogLevel
 import org.team5987.annotation.LoggedOutput
 
-object Conveyor : SubsystemBase(), ConveyorVelocityCommandFactory, SysIdable {
-
+object Spindexer : SubsystemBase(), ConveyorVelocityCommandFactory {
     private val mainMotor =
         UniversalTalonFX(
             MAIN_MOTOR_ID,
@@ -24,38 +19,22 @@ object Conveyor : SubsystemBase(), ConveyorVelocityCommandFactory, SysIdable {
             simGains = SIM_GAINS
         )
 
-    private val auxMotor =
-        UniversalTalonFX(
-                AUX_MOTOR_ID,
-                config = MOTOR_CONFIG,
-                gearRatio = GEAR_RATIO,
-                simGains = SIM_GAINS
-            )
-            .apply {
-                setControl(Follower(MAIN_MOTOR_ID, MotorAlignmentValue.Aligned))
-            } // TODO is using double motors ?
-
     private val velocityVoltage = VelocityVoltage(0.0)
-    private val voltageRequest = VoltageOut(0.0)
 
     private var setpoint = ConveyorVelocity.STOP
 
-    @LoggedOutput(LogLevel.COMP)
     val isAtSetpoint = Trigger {
         mainMotor.inputs.velocity.isNear(setpoint.velocity, SETPOINT_TOLERANCE)
     }
 
     override fun setTarget(value: ConveyorVelocity): Command = runOnce {
-        mainMotor.setControl(velocityVoltage.withVelocity(value.velocity))
         setpoint = value
-    }
-
-    override fun setVoltage(voltage: Voltage) {
-        mainMotor.setControl(voltageRequest.withOutput(voltage))
+        mainMotor.setControl(velocityVoltage.withVelocity(value.velocity))
     }
 
     override fun periodic() {
         mainMotor.periodic()
         Logger.recordOutput("Subsystems/$name/setpoint", setpoint.velocity)
+        Logger.recordOutput("Subsystems/$name/atSetpoint", isAtSetpoint)
     }
 }
--- src/main/kotlin/frc/robot/subsystems/netConveyor/SpindexerConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/netConveyor/SpindexerConstants.kt
@@ -1,22 +1,20 @@
-package frc.robot.subsystems.conveyor.netConveyor
+package frc.robot.subsystems.netConveyor
 
 import com.ctre.phoenix6.configs.MotorOutputConfigs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.signals.InvertedValue
 import edu.wpi.first.units.measure.AngularVelocity
 import frc.robot.lib.Gains
-import frc.robot.lib.LoggedNetworkGains
 import frc.robot.lib.createCurrentLimits
+import frc.robot.lib.extensions.amps
 import frc.robot.lib.extensions.rps
 import org.team5987.annotation.command_enum.CommandEnum
 
-val GAINS = LoggedNetworkGains("conveyorRealGains", 1.0) // TODO calibration
+val GAINS = Gains(1.0) // TODO calibration
 
 val SIM_GAINS = Gains(1.0)
 
 const val MAIN_MOTOR_ID = 0
-
-const val AUX_MOTOR_ID = 1
 
 const val GEAR_RATIO = 1.0
 
@@ -25,11 +23,11 @@ val SETPOINT_TOLERANCE = 0.5.rps
 val MOTOR_CONFIG =
     TalonFXConfiguration().apply {
         Slot0 = GAINS.toSlotConfig()
-        CurrentLimits = createCurrentLimits()
         MotorOutput =
             MotorOutputConfigs().apply {
                 Inverted = InvertedValue.Clockwise_Positive
             }
+        CurrentLimits = createCurrentLimits(40.amps, 5.0.amps)
     }
 
 @CommandEnum
