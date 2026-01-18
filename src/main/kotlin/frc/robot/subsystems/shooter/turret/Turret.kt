25661fcd9a029159cad3157f25c4be474e4902f3
Rakrakon <68773850+rakrakon@users.noreply.github.com>
2026-01-18 19:17:25 +0200

    Fix Turret

--- src/main/kotlin/frc/robot/subsystems/shooter/turret/Turret.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/turret/Turret.kt
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.shooter.turret
 
-import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC
 import com.ctre.phoenix6.hardware.CANcoder
 import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -11,8 +11,6 @@ import org.littletonrobotics.junction.AutoLogOutput
 import org.littletonrobotics.junction.Logger
 import org.littletonrobotics.junction.mechanism.LoggedMechanism2d
 import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d
-import org.team5987.annotation.LogLevel
-import org.team5987.annotation.LoggedOutput
 
 @AutoLogOutput(key = "Turret/mechanism")
 private var mechanism = LoggedMechanism2d(5.0, 5.0)
@@ -30,34 +28,31 @@ object Turret : SubsystemBase() {
         )
     private val absoluteEncoder = CANcoder(ENCODER_ID)
 
-    private val positionVoltageRequest: PositionVoltage = PositionVoltage(0.0)
-    @LoggedOutput(LogLevel.DEV) var setpoint = 0.deg
-    @LoggedOutput(LogLevel.DEV) val isAtSetpoint = Trigger {
+    private val positionTorqueCurrentFOC: PositionTorqueCurrentFOC = PositionTorqueCurrentFOC(0.0)
+    private var setpoint = 0.deg
+    val isAtSetpoint = Trigger {
         motor.inputs.position.isNear(setpoint, SETPOINT_TOLERANCE)
-    }
-
-    fun setAngle(angle: Angle) = runOnce {
-        setpoint = angle
-        motor.setControl(positionVoltageRequest.withPosition(angle))
-    }
-
-    fun setAngle(angleSupplier: () -> Angle) = run {
-        setpoint = angleSupplier()
-        motor.setControl(positionVoltageRequest.withPosition(setpoint))
-    }
-
-    fun resetAngle() = runOnce {
-        setpoint = 0.deg
-        motor.setControl(positionVoltageRequest.withPosition(0.0))
     }
 
     init {
         absoluteEncoder.configurator.apply(ENCODER_CONFIG)
     }
 
+    fun setAngle(angle: Angle) = runOnce {
+        setpoint = angle
+        motor.setControl(positionTorqueCurrentFOC.withPosition(angle))
+    }
+
+    fun setAngle(angleSupplier: () -> Angle) = run {
+        setpoint = angleSupplier()
+        motor.setControl(positionTorqueCurrentFOC.withPosition(setpoint))
+    }
+
     override fun periodic() {
         motor.periodic()
         ligament.setAngle(motor.inputs.position)
-        Logger.recordOutput("Turret", mechanism)
+        Logger.recordOutput("Subsystems/$name/mechanism", mechanism)
+        Logger.recordOutput("Subsystems/$name/setpoint", setpoint)
+        Logger.recordOutput("Subsystems/$name/atSetpoint", isAtSetpoint)
     }
 }
--- src/main/kotlin/frc/robot/subsystems/shooter/turret/TurretConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/turret/TurretConstants.kt
@@ -8,6 +8,7 @@ import com.ctre.phoenix6.signals.SensorDirectionValue
 import edu.wpi.first.units.measure.AngularVelocity
 import frc.robot.lib.Gains
 import frc.robot.lib.createCurrentLimits
+import frc.robot.lib.extensions.amps
 import frc.robot.lib.extensions.deg
 import frc.robot.lib.extensions.get
 import frc.robot.lib.extensions.rps
@@ -15,6 +16,7 @@ import org.team5987.annotation.command_enum.CommandEnum
 
 const val PORT = 0
 const val RATIO = 1.0
+const val ENCODER_RATIO = 1.0
 val SIM_GAINS = Gains(kP = 0.5, kD = 0.075)
 val REAL_GAINS = Gains(kP = 0.5, kD = 0.075)
 
@@ -38,7 +40,7 @@ val ENCODER_CONFIG =
             SensorDirectionValue.CounterClockwise_Positive
         MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.0
         MagnetSensor.MagnetOffset =
-            ABSOLUTE_ENCODER_OFFSET[rps] // What does it do?
+            ABSOLUTE_ENCODER_OFFSET[rps]
     }
 
 val CONFIG =
@@ -48,7 +50,6 @@ val CONFIG =
                 NeutralMode = NeutralModeValue.Coast
                 Inverted = InvertedValue.Clockwise_Positive
             }
-        CurrentLimits = createCurrentLimits()
         Slot0 =
             Slot0Configs().apply {
                 Slot0 = REAL_GAINS.toSlotConfig()
@@ -56,9 +57,11 @@ val CONFIG =
         Feedback =
             FeedbackConfigs().apply {
                 SensorToMechanismRatio = RATIO
+                RotorToSensorRatio = ENCODER_RATIO
                 FeedbackRemoteSensorID = ENCODER_ID
                 FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder
             }
+        CurrentLimits = createCurrentLimits(20.amps, 5.amps)
     }
 
 @CommandEnum
