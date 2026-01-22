fee8844855f431d63f2d735e18c0d48a4998d9a1
yahali <97111125+yakla@users.noreply.github.com>
2026-01-22 21:49:21 +0200

    spotless

--- src/main/kotlin/frc/robot/lib/stateMachine.kt
+++ src/main/kotlin/frc/robot/lib/stateMachine.kt
@@ -3,7 +3,6 @@ package frc.robot.lib
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import org.team5987.annotation.machine_states.MachineStates
 
-
 @MachineStates
 enum class ShooterState(
     override val context: Trigger.() -> Unit,
@@ -14,7 +13,5 @@ enum class ShooterState(
 }
 
 fun initStateMachine() {
-    ShooterState.entries.forEach {
-        it.stateTrigger()
-    }
+    ShooterState.entries.forEach { it.stateTrigger() }
 }
