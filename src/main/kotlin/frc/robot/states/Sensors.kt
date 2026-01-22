4787e53819dcbc9839f17231d1c37e8c91bd1a8c
yahali <97111125+yakla@users.noreply.github.com>
2026-01-22 21:13:08 +0200

    Sensors

---
+++ src/main/kotlin/frc/robot/states/Sensors.kt
@@ -0,0 +1,14 @@
+package frc.robot.states
+
+import com.ctre.phoenix6.configs.CANrangeConfiguration
+import edu.wpi.first.wpilibj2.command.button.Trigger
+import frc.robot.lib.unified_canrange.UnifiedCANRange
+
+enum class Sensors(port: Int) {
+    MidLevelSensor(1),
+    Spindexer1(2),
+    Spindexer2(3), ;
+
+    val canRange = UnifiedCANRange(port, configuration = CANrangeConfiguration())
+    val trigger = Trigger(canRange::isInRange)
+}
--- src/main/kotlin/frc/robot/states/sensors.kt
+++
@@ -1,7 +0,0 @@
-package frc.robot.states
-
-import frc.robot.lib.unified_canrange.UnifiedCANRange
-
-enum class sensors(){
-    CanClose()
-}
