9aa0da2c2598e68e605659f5ef7dbcd6b9adae57
nogav <noga.veksler@gmail.com>
2026-01-23 14:09:37 +0200

    FIX Sensors.kt

--- src/main/kotlin/frc/robot/states/sensors/Sensors.kt
+++ src/main/kotlin/frc/robot/states/sensors/Sensors.kt
@@ -3,24 +3,29 @@ package frc.robot.states.sensors
 import com.ctre.phoenix6.configs.CANrangeConfiguration
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.lib.extensions.cm
+import frc.robot.lib.extensions.m
 import frc.robot.lib.unified_canrange.UnifiedCANRange
+import org.team5987.annotation.LogLevel
+import org.team5987.annotation.LoggedOutput
 
-val FULL = 1.cm // change the value
-val HALF_FULL = 0.5.cm // change the value
-val HAS_FUEL = 0.0.cm // change the value
+private val FULL = 1.m // TODO: change the value
+private val HALF_FULL = 0.5.m // TODO: change the value
+private val HAS_FUEL = 5.cm // TODO: change the value
 
+private val spindexerSensor = UnifiedCANRange(SPINDEXER_SENSOR_PORT, configuration = SPINDEXER_SENSOR_CONFIG)
+private val topSensor = UnifiedCANRange(TOP_SENSOR_PORT, configuration = TOP_SWENSOR_CONFIG)
+private val auxTopSensor = UnifiedCANRange(AUX_TOP_SENSOR, configuration = AUX_TOP_SENSOR_CONFIG)
 
-val Spindexer= UnifiedCANRange(1, configuration = CANrangeConfiguration())
-val HighLevel1= UnifiedCANRange(2, configuration = CANrangeConfiguration())
-val HighLevel2= UnifiedCANRange(3, configuration = CANrangeConfiguration())
-
-
-
+@LoggedOutput(LogLevel.COMP)
 val isHalfFull: Trigger = Trigger {
-    (HighLevel1.inputs.distance > HALF_FULL).and(HighLevel2.inputs.distance > HALF_FULL
+    (topSensor.inputs.distance < HALF_FULL).and(
+        auxTopSensor.inputs.distance < HALF_FULL
     )
 }
+@LoggedOutput(LogLevel.COMP)
 val isFull: Trigger = Trigger {
-    (HighLevel1.inputs.distance == FULL).and(HighLevel2.inputs.distance == FULL)
+    (topSensor.inputs.distance <= FULL).and(auxTopSensor.inputs.distance <= FULL)
 }
-val hasFuel: Trigger = Trigger { Spindexer.inputs.distance > HAS_FUEL }
+
+@LoggedOutput(LogLevel.COMP)
+val hasFuel: Trigger = Trigger { spindexerSensor.inputs.distance < HAS_FUEL }
--- src/main/kotlin/frc/robot/states/sensors/SensorsConstsnts.kt
+++ src/main/kotlin/frc/robot/states/sensors/SensorsConstsnts.kt
@@ -1,1 +1,11 @@
 package frc.robot.states.sensors
+
+import com.ctre.phoenix6.configs.CANrangeConfiguration
+
+val SPINDEXER_SENSOR_PORT=1
+val TOP_SENSOR_PORT=2
+val AUX_TOP_SENSOR=3
+
+val SPINDEXER_SENSOR_CONFIG=CANrangeConfiguration()
+val TOP_SWENSOR_CONFIG= CANrangeConfiguration()
+val AUX_TOP_SENSOR_CONFIG= CANrangeConfiguration()
