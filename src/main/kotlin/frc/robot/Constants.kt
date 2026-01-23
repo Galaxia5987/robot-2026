e22b8a353b5374879b3d21cb81e063ef2a730350
AdarWa <adar.wasserman@gmail.com>
2026-01-23 15:49:25 +0200

    spotless

--- src/main/kotlin/frc/robot/Constants.kt
+++ src/main/kotlin/frc/robot/Constants.kt
@@ -1,20 +1,15 @@
 package frc.robot
 
-import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rectangle2d
-import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.units.measure.Distance
 import frc.robot.lib.IS_RED
 import frc.robot.lib.Mode
 import frc.robot.lib.extensions.get
 import frc.robot.lib.extensions.m
-import frc.robot.lib.getPose2d
-import frc.robot.lib.getTranslation2d
 import org.littletonrobotics.junction.LoggedRobot
 import org.team5987.annotation.LogLevel
 import org.team5987.annotation.LoggedOutput
-import java.awt.Dimension
 
 const val LOOP_TIME = 0.02 // [s]
 
@@ -40,7 +35,12 @@ val FIELD_WIDTH: Distance = 16.54.m
 val FIELD_HEIGHT: Distance = 4.03.m
 
 val ALLIANCE_ZONE
-    get() = Rectangle2d(
-        Translation2d(if (IS_RED) (FIELD_WIDTH - ALLIANCE_ZONE_WIDTH) else ALLIANCE_ZONE_WIDTH, ALLIANCE_ZONE_HEIGHT),
-        Translation2d(if(IS_RED) FIELD_WIDTH else 0.m, 0.m)
-    )
+    get() =
+        Rectangle2d(
+            Translation2d(
+                if (IS_RED) (FIELD_WIDTH - ALLIANCE_ZONE_WIDTH)
+                else ALLIANCE_ZONE_WIDTH,
+                ALLIANCE_ZONE_HEIGHT
+            ),
+            Translation2d(if (IS_RED) FIELD_WIDTH else 0.m, 0.m)
+        )
