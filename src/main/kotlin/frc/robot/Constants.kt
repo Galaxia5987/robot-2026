43101b99191dbf3e79e489af7992ac2c808ad89a
AdarWa <adar.wasserman@gmail.com>
2026-01-23 15:48:57 +0200

    Add `ALLIANCE_ZONE`

--- src/main/kotlin/frc/robot/Constants.kt
+++ src/main/kotlin/frc/robot/Constants.kt
@@ -1,10 +1,20 @@
 package frc.robot
 
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Rectangle2d
+import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.geometry.Translation2d
+import edu.wpi.first.units.measure.Distance
+import frc.robot.lib.IS_RED
 import frc.robot.lib.Mode
 import frc.robot.lib.extensions.get
+import frc.robot.lib.extensions.m
+import frc.robot.lib.getPose2d
+import frc.robot.lib.getTranslation2d
 import org.littletonrobotics.junction.LoggedRobot
 import org.team5987.annotation.LogLevel
 import org.team5987.annotation.LoggedOutput
+import java.awt.Dimension
 
 const val LOOP_TIME = 0.02 // [s]
 
@@ -22,3 +32,15 @@ val CURRENT_MODE: Mode
                 Mode.SIM
             }
         }
+
+val ALLIANCE_ZONE_WIDTH: Distance = 4.03.m
+val ALLIANCE_ZONE_HEIGHT: Distance = 8.07.m
+
+val FIELD_WIDTH: Distance = 16.54.m
+val FIELD_HEIGHT: Distance = 4.03.m
+
+val ALLIANCE_ZONE
+    get() = Rectangle2d(
+        Translation2d(if (IS_RED) (FIELD_WIDTH - ALLIANCE_ZONE_WIDTH) else ALLIANCE_ZONE_WIDTH, ALLIANCE_ZONE_HEIGHT),
+        Translation2d(if(IS_RED) FIELD_WIDTH else 0.m, 0.m)
+    )
