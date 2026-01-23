1a8531bada794cfbdb95e31ace8010dd6f946203
nogav <noga.veksler@gmail.com>
2026-01-23 15:58:01 +0200

    Spotless

--- src/main/kotlin/frc/robot/states/intaking/IntakingTriggers.kt
+++ src/main/kotlin/frc/robot/states/intaking/IntakingTriggers.kt
@@ -1,26 +1,20 @@
 package frc.robot.states.intaking
 
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.button.Trigger
-import frc.robot.spindexer
 import frc.robot.states.sensors.isHalfFull
-import frc.robot.subsystems.intake.extender.Extender
-import frc.robot.subsystems.spindexer.SpindexerVelocity
 
 val intakingTrigger = IntakingStates.INTAKING.trigger.onTrue(intaking())
 
-    val closedTrigger = IntakingStates.CLOSED.trigger.onTrue(closed())
+val closedTrigger = IntakingStates.CLOSED.trigger.onTrue(closed())
 
-    val openTrigger = IntakingStates.OPEN.trigger.onTrue(open())
+val openTrigger = IntakingStates.OPEN.trigger.onTrue(open())
 
-    val plummingTrigger = IntakingStates.PUMPING.trigger.onTrue(pumping())
+val plummingTrigger = IntakingStates.PUMPING.trigger.onTrue(pumping())
 
-fun canClose(): Command{
-    return if (isHalfFull.asBoolean==false){
+fun canClose(): Command {
+    return if (isHalfFull.asBoolean == false) {
         closed()
-    }else{
+    } else {
         open()
     }
 }
-
-
