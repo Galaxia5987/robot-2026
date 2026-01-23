861ffd8c5d5df41b971a4aa8a282a82e54359240
Elinor <elinor6almog@gmail.com>
2026-01-23 12:04:59 +0200

    Spotless

--- src/main/kotlin/frc/robot/states/spindexer/SpindexerCommands.kt
+++ src/main/kotlin/frc/robot/states/spindexer/SpindexerCommands.kt
@@ -1,18 +1,17 @@
 package frc.robot.states.spindexer
 
-import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.Commands.runOnce
-import frc.robot.lib.extensions.sec
 
 var isFeeding = false
 
 var isIntaking = false
 
-fun setFeeding(value : Boolean) = runOnce({ isFeeding =value })
-fun setIntaking(value : Boolean) = runOnce({ isIntaking = value })
+fun setFeeding(value: Boolean) = runOnce({ isFeeding = value })
 
-fun stop() = runOnce({
-    isFeeding= false
-    isIntaking = false
-})
+fun setIntaking(value: Boolean) = runOnce({ isIntaking = value })
 
+fun stop() =
+    runOnce({
+        isFeeding = false
+        isIntaking = false
+    })
--- src/main/kotlin/frc/robot/states/spindexer/SpindexerTriggers.kt
+++ src/main/kotlin/frc/robot/states/spindexer/SpindexerTriggers.kt
@@ -1,12 +1,12 @@
 package frc.robot.states.spindexer
 
-import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.spindexer
 import frc.robot.subsystems.spindexer.ConveyorVelocity
 import org.team5987.annotation.LogLevel
 import org.team5987.annotation.LoggedOutput
+
 val spinState =
     SpindexerStates.SPIN.trigger.onTrue(
         spindexer.setTarget(ConveyorVelocity.START)
@@ -17,7 +17,7 @@ val idleState =
     )
 
 @LoggedOutput(LogLevel.COMP)
-val spinRequested= Trigger{ isFeeding || isIntaking }
-    .onTrue(Commands.runOnce({SpindexerStates.SPIN.set()}))
-    .onFalse(Commands.runOnce({SpindexerStates.IDLE.set()}))
-
+val spinRequested =
+    Trigger { isFeeding || isIntaking }
+        .onTrue(Commands.runOnce({ SpindexerStates.SPIN.set() }))
+        .onFalse(Commands.runOnce({ SpindexerStates.IDLE.set() }))
