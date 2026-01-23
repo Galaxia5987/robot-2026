59aea1e41957430fa05d7f68e29e565f39cf9f1b
AdarWa <adar.wasserman@gmail.com>
2026-01-23 16:23:29 +0200

    Remove sensor from PreShooter.kt

--- src/main/kotlin/frc/robot/subsystems/shooter/pre_shooter/PreShooter.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/pre_shooter/PreShooter.kt
@@ -1,16 +1,12 @@
 package frc.robot.subsystems.shooter.pre_shooter
 
-import com.ctre.phoenix6.configs.CANrangeConfiguration
-import com.ctre.phoenix6.configs.ProximityParamsConfigs
 import com.ctre.phoenix6.controls.VelocityVoltage
 import edu.wpi.first.units.measure.AngularVelocity
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.lib.extensions.get
-import frc.robot.lib.extensions.m
 import frc.robot.lib.extensions.rps
-import frc.robot.lib.unified_canrange.UnifiedCANRange
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.littletonrobotics.junction.Logger
 
