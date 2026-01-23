49214f108a15688faa6896b80e861d2fd4a076f5
Elinor <elinor6almog@gmail.com>
2026-01-23 13:12:06 +0200

    Changed to start() and stop()

--- src/main/kotlin/frc/robot/states/spindexer/SpindexerTriggers.kt
+++ src/main/kotlin/frc/robot/states/spindexer/SpindexerTriggers.kt
@@ -4,17 +4,12 @@ import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.spindexer
 import frc.robot.subsystems.spindexer.ConveyorVelocity
+import frc.robot.subsystems.spindexer.Spindexer
 import org.team5987.annotation.LogLevel
 import org.team5987.annotation.LoggedOutput
 
-val spinState =
-    SpindexerStates.SPIN.trigger.onTrue(
-        spindexer.setTarget(ConveyorVelocity.START)
-    )
-val idleState =
-    SpindexerStates.IDLE.trigger.onTrue(
-        spindexer.setTarget(ConveyorVelocity.STOP)
-    )
+val spinState = spindexer.start()
+val idleState = Spindexer.stop()
 
 @LoggedOutput(LogLevel.COMP)
 val spinRequested =
