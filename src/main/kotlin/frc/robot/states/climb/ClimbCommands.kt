ed3b1247ee6eded175f72628cfe666a563ab21aa
yahali <97111125+yakla@users.noreply.github.com>
2026-01-25 12:15:23 +0200

    Finish Climb command

---
+++ src/main/kotlin/frc/robot/states/climb/ClimbCommands.kt
@@ -0,0 +1,28 @@
+package frc.robot.states.climb
+
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.ConditionalCommand
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand
+import frc.robot.field_constants.CLIMB_LOCATION
+import frc.robot.lib.extensions.toPose
+import frc.robot.spindexer
+import frc.robot.states.ExternalControl
+import frc.robot.subsystems.drive.profiledAlignToPose
+
+val overrideStates: Command = Commands.run({
+    SpindexerStates.IDLE.set()
+    IntakeState.IDLE.set()
+    ShooterState.IDLE.set()
+})
+
+fun climb(): Command = Commands.sequence(
+    overrideStates,
+    WaitUntilCommand(spindexer.isAtSetpoint),
+    ConditionalCommand(
+        Commands.none(),
+        profiledAlignToPose(CLIMB_LOCATION.toPose()),
+        ExternalControl.AlignmentOverride.trigger,
+    ),
+    ConditionalCommand(Climb.ENGAGED())
+).withName("climb")
