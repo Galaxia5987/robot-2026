a29577b7764dc38598f007f9c616cbaa3db226e5
nogav <noga.veksler@gmail.com>
2026-01-25 21:56:55 +0200

    Fix triggers

--- src/main/kotlin/frc/robot/states/spindexer/SpindexerStates.kt
+++ src/main/kotlin/frc/robot/states/spindexer/SpindexerStates.kt
@@ -3,10 +3,9 @@ package frc.robot.states.spindexer
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.lib.logged_output.LoggedOutputManager
 import org.team5987.annotation.LogLevel
-import org.team5987.annotation.LoggedOutput
 
 enum class SpindexerStates {
-    SPIN,
+    ACTIVE,
     IDLE;
 
     val trigger = Trigger { state == this }
