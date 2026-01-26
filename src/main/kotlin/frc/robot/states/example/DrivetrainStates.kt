41e943b6d84c2176da4a49d140fe9e66575cc49a
nogav <noga.veksler@gmail.com>
2026-01-26 20:10:22 +0200

    Fix stuff 2

--- src/main/kotlin/frc/robot/states/example/DrivetrainStates.kt
+++ src/main/kotlin/frc/robot/states/example/DrivetrainStates.kt
@@ -2,9 +2,7 @@ package frc.robot.states.example
 
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.lib.extensions.onTrue
-import frc.robot.lib.logged_output.LoggedOutputManager
 import org.littletonrobotics.junction.Logger
-import org.team5987.annotation.LogLevel
 
 enum class DrivetrainStates {
     DRIVING,
@@ -16,7 +14,7 @@ enum class DrivetrainStates {
     val trigger = Trigger { state == this }
 
     fun set() {
-        Logger.recordOutput("DrivertrainStates",state)
+        Logger.recordOutput("DrivertrainStates", state)
         state = this
     }
 }
