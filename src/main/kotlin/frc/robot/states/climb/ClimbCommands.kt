cb4659cdbf20d326d7e2f46c6ce438e420b131f8
AdarWa <adar.wasserman@gmail.com>
2026-01-27 20:45:34 +0200

    use only if

--- src/main/kotlin/frc/robot/states/climb/ClimbCommands.kt
+++ src/main/kotlin/frc/robot/states/climb/ClimbCommands.kt
@@ -2,7 +2,6 @@ package frc.robot.states.climb
 
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
-import edu.wpi.first.wpilibj2.command.ConditionalCommand
 import frc.robot.field_constants.CLIMB_LOCATION
 import frc.robot.lib.extensions.toPose
 import frc.robot.states.DriverOverrides
@@ -13,9 +12,6 @@ fun climb(): Command =
     Commands.runOnce({ Spindexer.stop() })
         .until(Spindexer.isAtSetpoint)
         .andThen(
-            ConditionalCommand(
-                Commands.none(),
-                profiledAlignToPose(CLIMB_LOCATION.toPose()),
-                DriverOverrides.AlignmentOverride.trigger,
-            )
+            profiledAlignToPose(CLIMB_LOCATION.toPose())
+                .onlyIf(DriverOverrides.AlignmentOverride.trigger.negate())
         )
