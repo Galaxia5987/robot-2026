6e22a63ac6d8ff8cef8915b82c66fbd88a2cb61b
yahali <97111125+yakla@users.noreply.github.com>
2026-01-28 20:48:14 +0200

    Forgot to change to onlyIf

--- src/main/kotlin/frc/robot/states/climb/ClimbCommands.kt
+++ src/main/kotlin/frc/robot/states/climb/ClimbCommands.kt
@@ -2,10 +2,8 @@ package frc.robot.states.climb
 
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
-import edu.wpi.first.wpilibj2.command.ConditionalCommand
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand
 import frc.robot.field_constants.CLIMB_LOCATION
-import frc.robot.lib.extensions.toPose
+import frc.robot.lib.extensions.not
 import frc.robot.states.DriverOverrides
 import frc.robot.states.intaking.IntakingStates
 import frc.robot.states.shooting.ShootingState
@@ -13,18 +11,18 @@ import frc.robot.states.spindexer.SpindexerStates
 import frc.robot.subsystems.climb.Climb
 import frc.robot.subsystems.drive.profiledAlignToPose
 
-val overrideStates: Command = Commands.run({
-    SpindexerStates.IDLE.set()
-    IntakingStates.CLOSED.set()
-    ShootingState.IDLE.set()
-})
+val overrideStates: Command =
+    Commands.run({
+        SpindexerStates.IDLE.set()
+        IntakingStates.CLOSED.set()
+        ShootingState.IDLE.set()
+    })
 
-fun climb(): Command = Commands.sequence(
-    overrideStates,
-    ConditionalCommand(
-        Commands.none(),
-        profiledAlignToPose(CLIMB_LOCATION),
-        DriverOverrides.AlignmentOverride.trigger,
-    ),
-    Climb.engaged()
-).withName("climb")
+fun climb(): Command =
+    Commands.sequence(
+        overrideStates,
+        profiledAlignToPose(CLIMB_LOCATION)
+            .onlyIf(!DriverOverrides.AlignmentOverride.trigger),
+        Climb.engaged()
+    )
+        .withName("climb")
