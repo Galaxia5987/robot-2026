99d7fe6d9e5b5ead3d21ad766ce122cefea08ede
Barak <rakrakon@gmail.com>
2026-02-02 13:03:21 +0200

    Calibrate PreShooter

--- src/main/kotlin/frc/robot/subsystems/shooter/pre_shooter/PreShooterConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/pre_shooter/PreShooterConstants.kt
@@ -12,7 +12,7 @@ import org.team5987.annotation.command_enum.CommandEnum
 
 val GAINS = Gains(1.0) // TODO calibration
 
-val SIM_GAINS = Gains(0.04, kV = 0.001)
+val SIM_GAINS = Gains(0.6, kV = 0.1)
 
 const val MAIN_MOTOR_ID = 0
 
