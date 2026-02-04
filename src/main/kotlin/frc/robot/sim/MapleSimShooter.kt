bd8db90af63fff9521cc08d1c399934a8e831910
Hadas <bar.hadas.bar@gmail.com>
2026-02-04 22:11:50 +0200

    add intialHeight, shooterPositionOnRobot and launchingSpeed

--- src/main/kotlin/frc/robot/sim/MapleSimShooter.kt
+++ src/main/kotlin/frc/robot/sim/MapleSimShooter.kt
@@ -1,14 +1,24 @@
 package frc.robot.sim
 
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.math.geometry.Translation3d
 import frc.robot.calculateVelocity
 import frc.robot.drive
+import frc.robot.field.HUB_LOCATION
 import frc.robot.lib.extensions.cm
 import frc.robot.lib.extensions.m
 import frc.robot.lib.extensions.mm
+import frc.robot.lib.extensions.distanceFromPoint
+import frc.robot.lib.extensions.m
+import frc.robot.lib.extensions.mm
 import frc.robot.lib.extensions.mps
+import frc.robot.lib.extensions.radians
 import frc.robot.lib.getTranslation2d
+import frc.robot.states.setpoints_manager.aimingSetpoint
 import frc.robot.subsystems.shooter.hood.Hood
+import frc.robot.subsystems.shooter.turret.angleFromRobotToHub
+import kotlinx.serialization.builtins.DoubleArraySerializer
 import org.ironmaple.simulation.seasonspecific.crescendo2024.NoteOnFly
 import java.util.function.Supplier
 import java.util.logging.Logger
@@ -17,11 +27,11 @@ class MapleSimShooter {
     private val noteOnFly =
         NoteOnFly(
             drive.pose.translation,
-            getTranslation2d(0.0, 0.0),
+            getTranslation2d(0.0, -50.0),//TODO: same here
             drive.chassisSpeeds,
             -drive.pose.rotation,
-            0.0.cm,
-            calculateVelocity(0.0, 0.0, 0.0).mps,
+            100.0.cm,//TODO:find the actual size
+            calculateVelocity(drive.pose.x, drive.pose.x, drive.pose.y).mps,
             Hood.inputs.position
         )
     private val Target =
@@ -31,21 +41,21 @@ class MapleSimShooter {
                     as Supplier<Translation3d?>?
             )
             .withTargetTolerance(Translation3d(10.0.cm, 10.cm, 10.cm))
-            .withHitTargetCallBack { print("Hit hub, +67 points!") }
+            .withHitTargetCallBack { print("Hit hub, +68 points!") }
 
     private val trajectory=noteOnFly
         .withProjectileTrajectoryDisplayCallBack(
             { pose3ds ->
-               Logger.recordOutput(
-                    "shooter/NoteProjectileSuccessfulShot",
-                    pose3ds.toTypedArray()
-                )
-            },
-            { pose3ds ->
-                Logger.recordOutput(
-                    "shooter/NoteProjectileUnsuccessfulShot",
-                    pose3ds.toTypedArray()
-                )
+                    Logger.recordOutput(
+                        "shooter/NoteProjectileSuccessfulShot",
+                        pose3ds.toTypedArray()
+                    )
+                },
+                { pose3ds ->
+                    Logger.recordOutput(
+                        "shooter/NoteProjectileUnsuccessfulShot",
+                        pose3ds.toTypedArray()
+                    )
             }
         )
 
