f8a2bda5e6c75c07d2155641d5e09944861fdcb1
nogav <noga.veksler@gmail.com>
2026-02-04 21:46:31 +0200

    Add Target and trajectory

--- src/main/kotlin/frc/robot/sim/MapleSimShooter.kt
+++ src/main/kotlin/frc/robot/sim/MapleSimShooter.kt
@@ -1,12 +1,17 @@
 package frc.robot.sim
 
+import edu.wpi.first.math.geometry.Translation3d
 import frc.robot.calculateVelocity
 import frc.robot.drive
 import frc.robot.lib.extensions.cm
+import frc.robot.lib.extensions.m
+import frc.robot.lib.extensions.mm
 import frc.robot.lib.extensions.mps
 import frc.robot.lib.getTranslation2d
 import frc.robot.subsystems.shooter.hood.Hood
 import org.ironmaple.simulation.seasonspecific.crescendo2024.NoteOnFly
+import java.util.function.Supplier
+import java.util.logging.Logger
 
 class MapleSimShooter {
     private val noteOnFly =
@@ -19,6 +24,29 @@ class MapleSimShooter {
             calculateVelocity(0.0, 0.0, 0.0).mps,
             Hood.inputs.position
         )
+    private val Target =
+        noteOnFly
+            .withTargetPosition(
+                Translation3d(4620.41.mm, 4034.63.mm, 1.82.m)
+                    as Supplier<Translation3d?>?
+            )
+            .withTargetTolerance(Translation3d(10.0.cm, 10.cm, 10.cm))
+            .withHitTargetCallBack { print("Hit hub, +67 points!") }
 
+    private val trajectory=noteOnFly
+        .withProjectileTrajectoryDisplayCallBack(
+            { pose3ds ->
+               Logger.recordOutput(
+                    "shooter/NoteProjectileSuccessfulShot",
+                    pose3ds.toTypedArray()
+                )
+            },
+            { pose3ds ->
+                Logger.recordOutput(
+                    "shooter/NoteProjectileUnsuccessfulShot",
+                    pose3ds.toTypedArray()
+                )
+            }
+        )
 
 }
