00d12779fc2d214c75b49d6f090b8bea3943a226
Hadas <bar.hadas.bar@gmail.com>
2026-02-05 18:06:50 +0200

    fix shit

--- src/main/kotlin/frc/robot/sim/MapleSimShooter.kt
+++ src/main/kotlin/frc/robot/sim/MapleSimShooter.kt
@@ -13,12 +13,9 @@ import frc.robot.lib.extensions.mm
 import frc.robot.lib.extensions.mps
 import frc.robot.lib.extensions.toRotation2d
 import frc.robot.lib.extensions.toTranslation3d
-import frc.robot.lib.getTranslation2d
 import frc.robot.states.setpoints_manager.shooting_modes.distanceFromGoal
 import frc.robot.subsystems.shooter.hood.Hood
 import frc.robot.subsystems.shooter.turret.Turret
-import frc.robot.subsystems.shooter.turret.turretAngleToHub
-import java.util.function.Supplier
 import org.ironmaple.simulation.seasonspecific.rebuilt2026.RebuiltFuelOnFly
 
 val HUB_HEIGHT = 177.cm // ONLY USED FOR MAPLE SIM!!!!
@@ -26,18 +23,30 @@ val HUB_HEIGHT = 177.cm // ONLY USED FOR MAPLE SIM!!!!
 object MapleSimShooter {
     fun createFuelOnFly(): RebuiltFuelOnFly {
         val robotSpeeds = drive.chassisSpeeds
-        val fuelOnFly = RebuiltFuelOnFly(
-            drive.pose.translation,
-            Translation2d((-116).mm, 220.5.mm).rotateBy(drive.pose.rotation),
-            robotSpeeds,
-            Turret.inputs.position.toRotation2d() + drive.pose.rotation,
-            0.47865.m,
-            calculateVelocity(distanceFromGoal[m], robotSpeeds.vxMetersPerSecond, robotSpeeds.vyMetersPerSecond).mps + 1.mps,
-            90.deg - Hood.inputs.position - 15.deg
-        )
+
+        val fuelOnFly =
+            RebuiltFuelOnFly(
+                drive.pose.translation,
+                Translation2d((-116).mm, 220.5.mm)
+                    .rotateBy(drive.pose.rotation),
+                robotSpeeds,
+                Turret.inputs.position.toRotation2d() + drive.pose.rotation,
+                0.47865.m,
+                if (distanceFromGoal > 10.m) 13.mps else
+                calculateVelocity(
+                        distanceFromGoal[m],
+                        robotSpeeds.vxMetersPerSecond,
+                        robotSpeeds.vyMetersPerSecond
+                    )
+                    .mps + 1.mps,
+                90.deg - Hood.inputs.position -15.deg
+            )
         fuelOnFly
             .withTargetPosition { HUB_LOCATION.toTranslation3d(HUB_HEIGHT) }
-            .withTargetTolerance(Translation3d(122.186335.cm, 122.186335.cm, 3.cm))
+            .withTargetTolerance(
+                Translation3d(122.186335.cm, 122.186335.cm, 3.cm)
+            )
         return fuelOnFly
     }
+
 }
