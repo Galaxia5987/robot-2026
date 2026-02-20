5bfea654c98174ea379ebd677885ec052c28ff4e
noamz <noamzil0510@gmail.com>
2026-02-20 14:48:41 +0200

    Leds Triggers Push For Checking

--- src/main/kotlin/frc/robot/subsystems/leds/Leds.kt
+++ src/main/kotlin/frc/robot/subsystems/leds/Leds.kt
@@ -4,17 +4,55 @@ import com.ctre.phoenix6.signals.RGBWColor
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
+import frc.robot.lib.extensions.get
+import frc.robot.lib.extensions.not
 import frc.robot.lib.extensions.rps
+import frc.robot.lib.extensions.volts
+import frc.robot.subsystems.intake.roller.Roller
 import frc.robot.subsystems.sensors.Sensors
 import frc.robot.subsystems.shooter.flywheel.Flywheel
 
+object LEDSubsystem : SubsystemBase() {
     private val candle = CANdle(0)
+    private val solidColorRequest = SolidColor(8, 399)
 
+    private fun setColor (color: RGBWColor) : Command = runOnce {
         candle.setControl(solidColorRequest.withColor(color))
     }
+
+    private fun flickeringOrange () : Command = run {
+        setColor(RGBWColor.fromHSV(15.0, 100.0, 100.0))
+            .withTimeout(0.75)
+        setColor(RGBWColor.fromHSV(0.0, 0.0, 0.0))
+            .withTimeout(0.75)
     }
+
+    private fun flickeringYellow() : Command = runOnce {
+        setColor(RGBWColor.fromHSV(50.0, 100.0, 100.0))
+            .withTimeout(0.75)
+        setColor(RGBWColor.fromHSV(0.0, 0.0, 0.0))
+            .withTimeout(0.75)
+    }
+
+    private fun staticWhite() : Command = runOnce {
+        setColor(RGBWColor.fromHSV(0.0, 0.0, 100.0))
+    }
+
+    private fun staticRed() : Command = runOnce {
         setColor(RGBWColor.fromHSV(0.0, 100.0, 100.0))
     }
 
+    private fun staticYellow() : Command = runOnce {
+        setColor(RGBWColor.fromHSV(50.0, 100.0, 100.0))
+    }
 
+    val isFull = Trigger(Sensors.isFull).onTrue(flickeringOrange())
+    val isEmpty = Trigger(Sensors.hasFuel).onFalse(staticWhite())
+    val isShooting = Trigger { Flywheel.inputs.velocity > 0.rps }.onTrue(staticRed())
+    val isLoading = Trigger {(Roller.inputs.velocity > 0.rps)
+        .and(isFull == !Sensors.isFull)}.onTrue(staticYellow())
+    val isNotFull = Trigger (!Sensors.cantCloseIntake
+        .and(!Sensors.isFull).onTrue(flickeringYellow()))
 }
+
+//check
