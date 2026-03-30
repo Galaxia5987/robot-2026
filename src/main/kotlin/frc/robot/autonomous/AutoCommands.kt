374812df5329838a7e7f7182696702f63d09654a
Workshop <rakrakon@gmail.com>
2026-03-30 14:34:02 +0300

    Some optimizations

--- src/main/kotlin/frc/robot/autonomous/AutoCommands.kt
+++ src/main/kotlin/frc/robot/autonomous/AutoCommands.kt
@@ -2,23 +2,33 @@ package frc.robot.autonomous
 
 import com.pathplanner.lib.auto.AutoBuilder
 import com.pathplanner.lib.events.EventTrigger
+import com.pathplanner.lib.path.PathConstraints
 import com.pathplanner.lib.path.PathPlannerPath
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import frc.robot.lib.BetterPoseEstimator
+import frc.robot.lib.extensions.deg_ps
+import frc.robot.lib.extensions.deg_ps_ps
 import frc.robot.lib.extensions.flipIfNeeded
 import frc.robot.lib.extensions.mirror
+import frc.robot.lib.extensions.mps
+import frc.robot.lib.extensions.mps_ps
 import frc.robot.states.intaking.IntakingStates
 import frc.robot.states.shooting.ShootingState
 import frc.robot.states.shooting.setShootInAuto
 import frc.robot.states.shooting.stopShootInAuto
 import frc.robot.subsystems.intake.roller.Roller
 
-private fun runPath(path: String, mirror: Boolean = false): Command {
+private fun runPath(path: String, mirror: Boolean = false, pathfindBefore: Boolean = false, pathfindingConstraints: PathConstraints? = null): Command {
     var path = PathPlannerPath.fromPathFile(path)
     if (mirror) {
         path = path.mirrorPath()
     }
+
+    if (pathfindBefore) {
+        return AutoBuilder.pathfindThenFollowPath(path, pathfindingConstraints!!)
+    }
+
     return AutoBuilder.followPath(path)
 }
 
@@ -55,16 +65,23 @@ val setRollerNormalCurrentLimit =
     EventTrigger("setRollerNormalCurrentLimit")
         .onTrue(Roller.setNormalCurrentLimits())
 
+private val DOUBLE_CYCLE_SHOOT_ON_MOVE_CONSTRAINTS = PathConstraints(
+    0.8.mps,
+    3.0.mps_ps,
+    540.deg_ps,
+    900.deg_ps_ps
+)
+
 fun depotBumpDoubleCycle(): Command =
     Commands.sequence(
         runPath("DepotSideFirstBumpCycle"),
         Commands.waitSeconds(3.0),
-        runPath("DepotSideSecondBumpCycle")
+        runPath("DepotSideSecondBumpCycle", pathfindBefore = true, pathfindingConstraints = DOUBLE_CYCLE_SHOOT_ON_MOVE_CONSTRAINTS)
     )
 
 fun outpostBumpDoubleCycle(): Command =
     Commands.sequence(
         runPath("DepotSideFirstBumpCycle", mirror = true),
         Commands.waitSeconds(3.0),
-        runPath("DepotSideSecondBumpCycle", mirror = true)
+        runPath("DepotSideSecondBumpCycle", mirror = true, pathfindBefore = true, pathfindingConstraints = DOUBLE_CYCLE_SHOOT_ON_MOVE_CONSTRAINTS)
     )
